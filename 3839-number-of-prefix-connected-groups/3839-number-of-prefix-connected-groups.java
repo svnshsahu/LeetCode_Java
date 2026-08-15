f04ff6e09d57341ef5dc53f6202c5059d55dc3c3
Shivansh Sahu <shivanshshahganj@gmail.com>
2026-08-15 15:00:04 +0530

    Time: 21 ms (49.79%), Space: 49.9 MB (22.75%) - LeetHub

--- 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.java
+++ 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.java
@@ -1,16 +1,14 @@
 class Solution {
     public int prefixConnected(String[] words, int k) {
-        String[] pre = new String[words.length];
-        for(int i=0;i<words.length;i++){
-            if(words[i].length()< k ) pre[i] = " ";
-            else pre[i] = words[i].substring(0 , k);
-        }
-        Arrays.sort(pre);
-        int count = 0 ;
         HashMap<String , Integer> map = new HashMap<>();
-        for(int i=0;i<pre.length;i++){
-            if(!pre[i].equals(" ")){
-                map.put( pre[i] , map.getOrDefault(pre[i] , 0) +1 );
+        for(int i=0;i<words.length;i++){
+            if(words[i].length()< k ) words[i] = " ";
+            else words[i] = words[i].substring(0 , k);
+        }
+        int count = 0 ;
+        for(int i=0;i<words.length;i++){
+            if(!words[i].equals(" ")){
+                map.put( words[i] , map.getOrDefault(words[i] , 0) +1 );
             }
         }
         for(String key : map.keySet()){
