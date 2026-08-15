9243eecebe38d068273458a7b27a5eeca01b78d2
Shivansh Sahu <shivanshshahganj@gmail.com>
2026-08-15 15:03:02 +0530

    Time: 25 ms (21.46%), Space: 50 MB (21.89%) - LeetHub

--- 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.java
+++ 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.java
@@ -1,18 +1,18 @@
 class Solution {
     public int prefixConnected(String[] words, int k) {
         HashMap<String , Integer> map = new HashMap<>();
+        int count = 0 ;
         for(int i=0;i<words.length;i++){
             if(words[i].length()< k ) words[i] = " ";
             else words[i] = words[i].substring(0 , k);
-        }
-        int count = 0 ;
-        for(int i=0;i<words.length;i++){
             if(!words[i].equals(" ")){
                 map.put( words[i] , map.getOrDefault(words[i] , 0) +1 );
+                if(map.get(words[i]) > 1){
+                    count++;
+                    map.put(words[i] , -5000);
+                } 
+
             }
-        }
-        for(String key : map.keySet()){
-            if(map.get(key) > 1) count++;
         }
         return count;
     }
